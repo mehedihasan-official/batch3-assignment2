82c5a0c4788cba7174127ca57a8871595201019d
Mehedi MehediSumu Sumu <mehedihasanshopnil.jr@gmail.com>
2026-01-19 22:20:21 +0600

    new update for the services operation get, create, update, delete function

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { ProductService } from '../services/product.services';
 import { productValidationSchema } from '../validations/product.validation';
+import { success } from 'zod';
 
 /**
  * Create a new product
@@ -22,6 +23,63 @@ const createProduct = async (req: Request, res: Response) => {
     });
   }
 };
+
+
+//  Get All Products
+const getAllProduct = async (req:Request, res:Response) => {
+    try{
+        const result = await ProductService.getAllProductsFromDB();
+
+        res.status(200).json({
+            success: true,
+            message: "Products fetched successfully",
+            data: result,
+        }) 
+    } catch (err){
+            res.status(500).json({
+                success: false,
+                message: 'Failed to fetch products'
+            })
+        }
+
+}
+
+// Get Single Product
+const getSingleProduct = async (req: Request, res: Response) => {
+    try{
+        const { id } = req.params;
+        const result = await ProductService.getSingleProductFromDB(id);
+
+        res.status(200).json({
+            success: true,
+            message: "Product fetched successfully",
+            data: result
+        })
+    } catch (err){
+        res.status(500).json({
+                success: false,
+                message: 'Failed to fetch product'
+            })
+    }
+}
+
+
+// Update Product
+const updateProduct = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const validationData = productValidationSchema.parse(req.body);
+
+    const result = await ProductService.updateProductIntoDB( 
+        id,
+        validationData
+    );
+
+    res.status(200).json({
+        success: true,
+        message: "Product updated successfully",
+        data: result
+    })
+}
 
 export const ProductController = {
   createProduct,
