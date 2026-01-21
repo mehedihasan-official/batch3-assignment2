59ac9ec0fa1b6f6bf70f472bb50daf0a9e9d083b
Mehedi MehediSumu Sumu <mehedihasanshopnil.jr@gmail.com>
2026-01-21 21:45:11 +0600

    new update for the app file

--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (_req, res) => {
+  res.send('API is running 🚀');
+});
+
 // routes
 app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
